0e03686cf9ecf6272bfc0d9b2e4c236e34a24982
hgg <postmaster@aachenerkinder.de>
2021-04-05 07:42:22 +0200

    VERBESSERUNG: css-Dateien in functions.php statt in style.css eingebunden

--- haurand/customizr-pro-childtheme/functions.php
+++ haurand/customizr-pro-childtheme/functions.php
@@ -9,8 +9,9 @@
 function child_theme_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'additional-style', get_stylesheet_directory_uri() . '/parallax_scrolling.css' );
-// die folgende Zeile ist wohl nicht notwendig:
-//wp_enqueue_style( 'child-theme-css', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
+  wp_enqueue_style( 'additional-style', get_stylesheet_directory_uri() . '/posts_in_site.css' );
+// die folgende Zeile ist wohl nicht notwendig: (wieder aktiviert wegen GTMetrix, hgg, 5.4.2021)
+  wp_enqueue_style( 'child-theme-css', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
 
 }
 add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
