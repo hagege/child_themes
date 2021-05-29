94a19c1a6af2d7b6f19a36c39bb764faa8259d76
hgg <postmaster@aachenerkinder.de>
2021-05-29 22:11:50 +0200

    Script zur lokalen Wiederherstellung von ackids

---
+++ wp_cli/wp_updraftplus_restore_vortrag.sh
@@ -0,0 +1,6 @@
+php ../wp-cli.phar updraftplus existing_backups
+php ../wp-cli.phar updraftplus get_latest_full_backup
+# Doku: https://updraftplus.com/wp-cli-updraftplus-documentation/
+# hat funktioniert:
+# php ../wp-cli.phar updraftplus restore faf8caf9662d --components="db"
+php ../wp-cli.phar updraftplus restore 89020effac4f --components="uploads"
