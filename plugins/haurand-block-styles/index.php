43ee025a2868b6f925cef56e8356c1b353e73a87
hgg <postmaster@aachenerkinder.de>
2021-10-07 17:14:32 +0200

    hgg-fse-theme um theme.json erweitert; haurand-block-styles aktualisiert

--- plugins/haurand-block-styles/index.php
+++ plugins/haurand-block-styles/index.php
@@ -1,29 +1,78 @@
 <?php
-/**
+/*
 Plugin Name: Haurand Block Styles
 Plugin URI: https://haurand.com
-Description: Box-Variante für den Absatz-Block
-Version: 1.0
+Description: Beipiel für Block-Styles bei Headern. Header werden verschiedenfarbig dargestellt, Auswahl bei den Eigenschaften
 Author: Hans-Gerd Gerhards
-*/
-/**
-Register Custom Block Styles
-*/
-if ( ! defined( 'ABSPATH' ) ) exit;
-/**
-Enqueue Block Styles Javascript
-*/
-function haurand_absatz_box() {
-	wp_enqueue_script(
-		'haurand-block-styles-js', plugins_url( '/blocks.js', __FILE__ ), array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ), filemtime( plugin_dir_path( __FILE__ ) . '/blocks.js' )
-	);
-}
-add_action( 'enqueue_block_editor_assets', 'haurand_absatz_box' );
+Author URI: https://haurand.com (Quelle: Automattic: https://github.com/Automattic/gutenberg-block-styles/)
+Version: 1.0
+
+
+
 
 /**
-Enqueue Block Styles Stylesheet
-*/
-function haurand_absatz_box_styles() {
-	wp_enqueue_style( 'haurand-block-styles-css', plugins_url( '/block-styles.css', __FILE__ ));
+ * 1. Register Custom Block Styles
+ */
+ 
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( function_exists( 'register_block_style' ) ) {
+	function haurand_block_styles() {
+		/**
+		 * Register stylesheet
+		 */
+		wp_register_style(
+			'block-styles-stylesheet',
+			plugins_url( 'style.css', __FILE__ ),
+			array(),
+			'1.2'
+		);
+
+		
+		/* Headings */ 
+        register_block_style(
+			'core/heading',
+			array(
+	           'name'          => 'gradient-red-headline',
+	           'label'         => 'Gradient rote Überschrift',
+			   'style_handle'  => 'block-styles-stylesheet',
+          	)
+		);       
+        register_block_style(
+			'core/heading',
+			array(
+	           'name'          => 'gradient-blue-headline',
+	           'label'         => 'Gradient blaue Überschrift',
+			   'style_handle'  => 'block-styles-stylesheet',
+          	)
+		);       
+        register_block_style(
+			'core/heading',
+			array(
+	           'name'          => 'gradient-green-headline',
+	           'label'         => 'Gradient grüne Überschrift',
+			   'style_handle'  => 'block-styles-stylesheet',
+          	)
+		);       
+        register_block_style(
+			'core/heading',
+			array(
+	           'name'          => 'gradient-yellow-headline',
+	           'label'         => 'Gradient gelbe Überschrift',
+			   'style_handle'  => 'block-styles-stylesheet',
+          	)
+		);       
+        register_block_style(
+			'core/heading',
+			array(
+	           'name'          => 'gradient-grey-headline',
+	           'label'         => 'Gradient graue Überschrift',
+			   'style_handle'  => 'block-styles-stylesheet',
+          	)
+		);       
+	}
+    
+
+	add_action( 'init', 'haurand_block_styles' );
 }
-add_action( 'enqueue_block_assets', 'haurand_absatz_box_styles' );
+
