8d44fc4023ec9fc77480ee2ba2470d7b0a2ccc04
hgg <postmaster@aachenerkinder.de>
2022-01-16 12:08:44 +0100

    Block Pattern für 2 Kacheln mit links oben und rechts unten abgerundeten Ecken

--- eilendorf_net/generatepress_child/functions.php
+++ eilendorf_net/generatepress_child/functions.php
@@ -12,3 +12,68 @@ function generatepress_child_enqueue_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_scripts', 100 );
+
+
+/* ------------------------------ */
+/* eigene Kategorie               */
+/* ------------------------------ */
+if ( function_exists( 'register_block_pattern_category' ) ) {
+  register_block_pattern_category(
+    'haurand',
+    array( 'label' => __( 'Forms', 'text-domain' ) )
+ );
+}
+
+
+function haurand_register_block_categories() {
+  if ( class_exists( 'WP_Block_Patterns_Registry' ) ) {
+  register_block_pattern_category(
+   'haurand',
+   array( 'label' => _x( 'Vorlagen Haurand', 'Block pattern category', 'haurand' ) )
+   );
+  }
+ }
+ add_action( 'init', 'haurand_register_block_categories' );
+
+
+ register_block_pattern(
+	/* https://developer.wordpress.org/block-editor/reference-guides/block-api/block-patterns/ */
+	/* maskieren ist nicht mehr nötig, wenn man einfache Hochkommatas nimmt */
+   'one-tile-card-pattern',
+     array(
+     'title' => __( 'One column with pictures', 'eine-kachel-block-pattern' ),
+     'description' => _x( 'One column with pictures (tiles)', 'Block pattern description', 'eine-kachel-block-pattern' ),
+     'categories'  => array('haurand'),
+     'content'     =>
+        '<!-- wp:columns -->
+			<div class="wp-block-columns"><!-- wp:column {"className":"kacheln_abgerundet"} -->
+			<div class="wp-block-column kacheln_abgerundet"><!-- wp:image {"align":"center","id":898,"sizeSlug":"full","linkDestination":"none","className":"kachelbild_abgerundet"} -->
+			<div class="wp-block-image kachelbild_abgerundet"><figure class="aligncenter size-full"><img src="https://www.eilendorf.net/wp-content/uploads/2020/09/Pannhaus-1.jpg" alt="Eifelausläufer" class="wp-image-898"/></figure></div>
+			<!-- /wp:image -->
+
+			<!-- wp:heading {"textAlign":"center"} -->
+			<h2 class="has-text-align-center">Über Uns</h2>
+			<!-- /wp:heading -->
+
+			<!-- wp:list -->
+			<ul><li><a href="https://test.hochzeitwebseiten.de/stadtteilkonferenz/">Stadtteilkonferenz</a></li><li><a href="https://eilendorf.net/eilendorf-netz/">Eilendorf.net</a></li><li><a href="https://eilendorf.net/projekte/">Projekte</a></li></ul>
+			<!-- /wp:list --></div>
+			<!-- /wp:column -->
+
+			<!-- wp:column {"className":"kacheln_abgerundet"} -->
+			<div class="wp-block-column kacheln_abgerundet"><!-- wp:image {"align":"center","id":409,"sizeSlug":"full","linkDestination":"none","className":"kachelbild_abgerundet"} -->
+			<div class="wp-block-image kachelbild_abgerundet"><figure class="aligncenter size-full"><img src="https://www.eilendorf.net/wp-content/uploads/2020/09/Pannhaus-1.jpg" alt="eilendorf.net" class="wp-image-409"/></figure></div>
+			<!-- /wp:image -->
+
+			<!-- wp:heading {"textAlign":"center"} -->
+			<h2 class="has-text-align-center">Service</h2>
+			<!-- /wp:heading -->
+
+			<!-- wp:list -->
+			<ul><li><a href="https://eilendorf.net/events/">Termine</a></li><li><a href="https://eilendorf.net/eilendorf-karte/?nocache">Eilendorfkarte</a></li><li><a href="https://www.eilendorf.net/karteneintrag/">Karteneinträge</a></li><li><a href="https://eilendorf.net/veranstaltungseingabe/">Terminerfassung</a></li><li><a href="https://eilendorf.net/baudenkmaeler/">Baudenkmäler </a></li><li><a href="https://eilendorf.net/kontakt/">Kontakt</a></li></ul>
+			<!-- /wp:list --></div>
+			<!-- /wp:column --></div>
+			<!-- /wp:columns -->',
+ )
+);
+
