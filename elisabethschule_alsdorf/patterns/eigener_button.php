476a4348f2cfc4d394d0832644110c1bee36fd31
hgg <postmaster@aachenerkinder.de>
2022-09-03 12:50:41 +0200

    Eigener Button als Pattern erstellt

---
+++ elisabethschule_alsdorf/patterns/eigener_button.php
@@ -0,0 +1,20 @@
+<?php
+ /**
+  * Title: Eigener Button
+  * Slug: generatepress/eigener_button
+  * Block Types: core/post-content
+  * Post Types: post
+  * Categories: Vorlagen Haurand, featured
+  */
+?>
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:generateblocks/button-container {"uniqueId":"7efb7c11","alignment":"center","isDynamic":true,"blockVersion":2} -->
+<!-- wp:generateblocks/button {"uniqueId":"8f712787","hasUrl":false,"backgroundColor":"var(\u002d\u002dglobal-color-10)","backgroundColorHover":"var(\u002d\u002dglobal-color-12)","textColor":"#ffffff","textColorHover":"var(\u002d\u002daccent-semi-transparent)","borderColor":"","borderColorHover":"","paddingTop":"15","paddingRight":"20","paddingBottom":"15","paddingLeft":"20","borderRadiusTopRight":"10","borderRadiusBottomRight":"10","borderRadiusBottomLeft":"10","borderRadiusTopLeft":"10","blockVersion":2} -->
+<span class="gb-button gb-button-8f712787 gb-button-text">Hier klicken</span>
+<!-- /wp:generateblocks/button -->
+<!-- /wp:generateblocks/button-container --></div>
+<!-- /wp:group -->
