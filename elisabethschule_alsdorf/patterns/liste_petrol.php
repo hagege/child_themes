ee478f846f1f4719ee0e2070bce83c042bc45798
hgg <postmaster@aachenerkinder.de>
2022-09-13 11:54:28 +0200

    Schriftarten lokal eingebunden, neuer Pattern für Listen, CSS-Klasse für Liste

---
+++ elisabethschule_alsdorf/patterns/liste_petrol.php
@@ -0,0 +1,17 @@
+<?php
+ /**
+  * Title: Liste Petrol
+  * Slug: generatepress/liste-petrol
+  * Block Types: core/post-content
+  * Post Types: post
+  * Categories: Vorlagen Haurand, featured
+  */
+?>
+<!-- wp:list {"className":"liste_gruener_punkt"} -->
+<ul class="liste_gruener_punkt">
+    <li>Ist ein Kind erkrankt muss es von den Eltern/Erziehungsberechtigten zwischen 7 Uhr und 7:50 Uhr in der Schule telefonisch entschuldigt werden. In dieser Zeit ist die Schule immer telefonisch erreichbar. Die Erziehungsberechtigten teilen in der telefonischen Nachricht mit, wie lange der Schüler voraussichtlich fehlen wird. Eine mündliche Entschuldigung über Geschwisterkinder, Freunde und Bekannte ist nicht zulässig, die Abgabe einer von Ihnen erstellten schriftlichen Entschuldigung über diese Personen schon.</li>
+    <li>Der Schüler wird bei bis zu 2 Fehltagen durch einen Erziehungsberechtigten zusätzlich zur telefonischen Mitteilung (nachträglich) durch eine schriftliche Notiz entschuldigt.</li>
+    <li>Ab dem 3. Krankheitstag muss das Fehlen des Schülers in der Schule ärztlich attestiert werden.</li>
+    <li>Die schriftliche Entschuldigung der Erziehungsberechtigten oder die ärztliche Bescheinigung ist umgehend, d.h. am Tag der Rückkehr des Schülers beim Klassenlehrer abzugeben.</li>
+</ul>
+<!-- /wp:list -->
