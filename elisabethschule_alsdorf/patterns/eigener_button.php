50d9591df33a96bb40702e838628d522f512fe55
hgg <postmaster@aachenerkinder.de>
2022-09-18 17:35:21 +0200

    verschiedene Verbesserungen bzeüglich Suche, Kalender, Links in der sidebar und Block Pattern Buttons

--- elisabethschule_alsdorf/patterns/eigener_button.php
+++ elisabethschule_alsdorf/patterns/eigener_button.php
@@ -9,8 +9,8 @@
 ?>
 <!-- wp:group -->
 <div class="wp-block-group"><!-- wp:generateblocks/button-container {"uniqueId":"7efb7c11","alignment":"center","isDynamic":true,"blockVersion":2} -->
-<!-- wp:generateblocks/button {"uniqueId":"8f712787","hasUrl":false,"backgroundColor":"var(\u002d\u002dglobal-color-10)","backgroundColorHover":"var(\u002d\u002dglobal-color-12)","textColor":"#ffffff","textColorHover":"var(\u002d\u002daccent-semi-transparent)","borderColor":"","borderColorHover":"","paddingTop":"15","paddingRight":"20","paddingBottom":"15","paddingLeft":"20","borderRadiusTopRight":"10","borderRadiusBottomRight":"10","borderRadiusBottomLeft":"10","borderRadiusTopLeft":"10","blockVersion":2} -->
-<span class="gb-button gb-button-8f712787 gb-button-text">Hier klicken</span>
+<!-- wp:generateblocks/button {"uniqueId":"8f712787","hasUrl":false,"backgroundColor":"var(\u002d\u002dglobal-color-10)","backgroundColorHover":"var(\u002d\u002dglobal-color-11)","textColor":"#ffffff","textColorHover":"var(\u002d\u002daccent-semi-transparent)","borderColor":"","borderColorHover":"","paddingTop":"15","paddingRight":"20","paddingBottom":"15","paddingLeft":"20","borderRadiusTopRight":"10","borderRadiusBottomRight":"10","borderRadiusBottomLeft":"10","borderRadiusTopLeft":"10","blockVersion":2} -->
+<span class="gb-button gb-button-8f712787 gb-button-text"><strong>hier ist der Link</strong></span>
 <!-- /wp:generateblocks/button -->
 <!-- /wp:generateblocks/button-container -->
 
