48c7196265101072c50cd0c6044f8c6219495275
hgg <postmaster@aachenerkinder.de>
2023-02-04 08:09:41 +0100

    Plugin entsprechend aktualisiert

--- rabenhorst/readme.txt
+++ rabenhorst/readme.txt
@@ -4,7 +4,7 @@ Donate link: https://haurand.com/
 Tags: category list
 Requires at least: 4.7
 Tested up to: 6.2
-Stable tag: 1.0
+Stable tag: 1.1
 Requires PHP: 7.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
--- rabenhorst/shortcode_categories_buttons.php
+++ rabenhorst/shortcode_categories_buttons.php
@@ -2,22 +2,28 @@
 /*
 Plugin Name: All Used Category Names
 Description: Displays a list of all used category names in your WordPress site
-Version: 1.0
+Version: 1.1
 Author: Hans-Gerd Gerhards
 */
+
+/*----------------------------------------------------------------*/
+/* Start: Shortcode für Kategorienliste - Aufruf mit [my_category_list]
+/* Datum: 04.2.2023
+/* Autor: hgg
+/*----------------------------------------------------------------*/
 
 function my_category_list_shortcode() {
     $args = array(
         'orderby' => 'name',
-        'hide_empty' => 0
+        'hide_empty' => 1
     );
     $my_categories = get_categories($args);
-    $my_output = '<div>';
+    $my_output = '<p class="beitrags_kategorien">';
     foreach($my_categories as $my_category) {
-        // $output .= '<a href="' . get_category_link($my_category->term_id) . '">' . $my_category->name . ' (' . $my_category->count . ')</a>' . '   ';
-		$my_output .= '<a class="my_category_list" href="' . get_category_link($my_category->term_id) . '">' . $my_category->name . '</a>' . '   ';
+        // $my_output .= '<a class="my_category_list" href="' . get_category_link($my_category->term_id) . '">' . $my_category->name . ' (' . $my_category->count . ')</a>' . '   ';
+        $my_output .= '<a class="my_category_list" href="' . get_category_link($my_category->term_id) . '">' . $my_category->name . '</a>' . '   ';
     }
-    $my_output .= '</div>';
-    return $my_output;
-}
-add_shortcode('my_category_list', 'my_category_list_shortcode');
+    $my_output .= '</p>';
+    return "<h3>Beitrags-Kategorien</h3>$my_output";
+  }
+  add_shortcode('my_category_list', 'my_category_list_shortcode');
