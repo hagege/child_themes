690a00b7cad29529254ac6707452fb9401625633
hgg <postmaster@aachenerkinder.de>
2023-03-16 17:48:30 +0100

    styles and patterns hgg_theme_2023

---
+++ hgg_theme_2023/patterns/footer-powered-by.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Powered by pattern
+ * Slug: block-course-theme/powered-by
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:site-title {"level":0} /-->
+
+<!-- wp:paragraph -->
+<p class="has-text-align-right"><?php echo __('Proudly powered by', 'new-blank-theme'); ?> <a href="https://wordpress.org" rel="nofollow">WordPress</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
--- hgg_theme_2023/patterns/hero.php
+++ hgg_theme_2023/patterns/hero.php
@@ -1,23 +1,23 @@
 <?php
 /**
  * Title: Hero
- * Slug: hgg_theme_2023/hero
- * Categories: featured, hgg_theme_2023
+ * Slug: block-course-theme/hero
+ * Categories: featured, block-course-theme
  */
 ?>
-<!-- wp:cover {"overlayColor":"contrast"} -->
-<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<!-- wp:cover {"overlayColor":"contrast","align":"full"} -->
+<div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"2.5rem"}},"layout":{"type":"constrained","wideSize":"%","contentSize":"75%"}} -->
 <div class="wp-block-group"><!-- wp:heading {"textAlign":"center"} -->
-<h2 class="wp-block-heading has-text-align-center">Welcome to my Site</h2>
+<h2 class="has-text-align-center"><?php _e( 'Welcome to My Site', 'themeslug' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center">Das ist ein Absatz</p>
+<p class="has-text-align-center"><?php _e( "This is my little home away from home. Here, you will get to know me.  I'll share my likes, hobbies, and more.  Every now and then, I'll even have something interesting to say in a blog post.", 'themeslug' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","backgroundColor":"contrast","textColor":"base","className":"is-style-outline"} -->
-<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-base-color has-contrast-background-color has-text-color has-background has-text-align-center wp-element-button">Jetzt Klicken</a></div>
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button"><?php _e( 'See My Popular Posts →', 'themeslug' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div></div>
