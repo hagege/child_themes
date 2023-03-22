a3c4c2bdaba3766e64f34d61b4fc0e782956751d
hgg <postmaster@aachenerkinder.de>
2023-03-22 11:38:10 +0100

    categories for each custom website

--- plugins/haurand_patterns/haurand_patterns.php
+++ plugins/haurand_patterns/haurand_patterns.php
@@ -48,14 +48,14 @@ add_action( 'init', 'patterns_haurand_register_block_categories' );
    
 
 /* eigene Kategorie Haurand */            
-function custom_websites_haurand_register_block_categories() {
+function custom_eks_haurand_register_block_categories() {
   if ( class_exists( 'WP_Block_Patterns_Registry' ) ) {
     register_block_pattern_category(
-     'Custom Websites Haurand', array( 'label' => _x( 'Custom Websites Patterns Haurand', 'Block pattern category for custom websites' ) )
+     'EKS Haurand', array( 'label' => _x( 'EKS Patterns Haurand', 'Block pattern category EKS' ) )
       );
     }
   }
-  add_action( 'init', 'custom_websites_haurand_register_block_categories' );
+  add_action( 'init', 'custom_eks_haurand_register_block_categories' );
  
 
 
@@ -177,7 +177,7 @@ register_block_pattern(
     array(
     'title' => __( 'Eigener Button', 'eigener_button' ),
     'description' => _x( 'Eigener Button', 'Eigener Button', 'eigener_button' ),
-    'categories'  => array('Custom Websites Haurand'),
+    'categories'  => array('EKS Haurand'),
     'content'     =>
        '<!-- wp:group -->
        <div class="wp-block-group"><!-- wp:generateblocks/button-container {"uniqueId":"b6923a0f","alignment":"center","isDynamic":true,"blockVersion":2} -->
@@ -202,7 +202,7 @@ register_block_pattern(
     array(
     'title' => __( 'H2 Petrol mit Icon', 'H2_Petrol_mit_Icon' ),
     'description' => _x( 'H2 Petrol mit Icon', 'H2 Petrol mit Icon', 'H2_Petrol_mit_Icon' ),
-    'categories'  => array('Custom Websites Haurand'),
+    'categories'  => array('EKS Haurand'),
     'content'     =>
        '<!-- wp:generateblocks/headline {"uniqueId":"f55e21fc","alignment":"center","marginBottom":"60","hasIcon":true,"iconColor":"var(\u002d\u002dglobal-color-10)","iconLocation":"above","iconPaddingRight":"","iconPaddingBottom":"0.4","iconSize":40,"iconSizeUnit":"px"} -->
 		<h2 class="gb-headline gb-headline-f55e21fc"><span class="gb-icon"><svg viewBox="0 0 17 21.001" xmlns=""><path d="M15.5 21c-.307 0-.612-.094-.872-.279L8.5 16.344l-6.128 4.377A1.499 1.499 0 010 19.5v-16C0 1.57 1.57 0 3.5 0h10C15.43 0 17 1.57 17 3.5v16a1.502 1.502 0 01-1.5 1.5zm-7-8a1.5 1.5 0 01.872.279L14 16.585V3.5a.5.5 0 00-.5-.5h-10a.5.5 0 00-.5.5v13.085l4.628-3.306A1.5 1.5 0 018.5 13z"></path></svg></span><span class="gb-headline-text"><strong>Soziale Arbeit an Schulen und Schulsozialarbeit</strong></span></h2>
@@ -219,7 +219,7 @@ register_block_pattern(
     array(
     'title' => __( 'H2 Petrol', 'H2_Petrol' ),
     'description' => _x( 'H2 Petrol', 'H2 Petrol', 'H2_Petrol' ),
-    'categories'  => array('Custom Websites Haurand'),
+    'categories'  => array('EKS Haurand'),
     'content'     =>
        '<!-- wp:generateblocks/headline {"uniqueId":"da450a80","alignment":"left","textColor":"","borderColor":"var(\u002d\u002dglobal-color-10)","highlightTextColor":"","paddingLeft":"20","borderSizeLeft":"6","iconColor":"var(\u002d\u002dglobal-color-10)","iconSize":34,"iconSizeUnit":"px"} -->
 		<h2 class="gb-headline gb-headline-da450a80 gb-headline-text">The History of Donate</h2>
@@ -236,7 +236,7 @@ register_block_pattern(
     array(
     'title' => __( 'Liste Petrol', 'Liste_Petrol' ),
     'description' => _x( 'Liste_Petrol', 'Liste Petrol', 'H2_Petrol' ),
-    'categories'  => array('Custom Websites Haurand'),
+    'categories'  => array('EKS Haurand'),
     'content'     =>
        '<!-- wp:list {"className":"liste_gruener_punkt"} -->
 		<ul class="liste_gruener_punkt">
@@ -256,7 +256,7 @@ register_block_pattern(
     array(
     'title' => __( 'Text mit Überschrift H2 und 2 Bildern', 'text_mit_ueberschrift_h2_und_2_bildern' ),
     'description' => _x( 'Text mit Überschrift H2 und 2 Bildern', 'Text mit Überschrift H2 und 2 Bildern', 'text_mit_ueberschrift_h2_und_2_bildern' ),
-    'categories'  => array('Custom Websites Haurand'),
+    'categories'  => array('EKS Haurand'),
     'content'     =>
        '<!-- wp:group -->
 		<div class="wp-block-group"><!-- wp:generateblocks/headline {"uniqueId":"357cc05f","alignment":"left","textColor":"","borderColor":"var(\u002d\u002dglobal-color-10)","highlightTextColor":"","paddingLeft":"20","borderSizeLeft":"6","iconColor":"var(\u002d\u002dglobal-color-10)","iconSize":34,"iconSizeUnit":"px"} -->
@@ -289,7 +289,7 @@ register_block_pattern(
     array(
     'title' => __( 'Text mit Überschrift H2 und Bild 2-spaltig', 'text_mit_ueberschrift_h2_und_bild_2_spaltig' ),
     'description' => _x( 'Text mit Überschrift H2 und Bild 2-spaltig', 'Text mit Überschrift H2 und Bild 2-spaltig', 'text_mit_ueberschrift_h2_und_bild_2_spaltig' ),
-    'categories'  => array('Custom Websites Haurand'),
+    'categories'  => array('EKS Haurand'),
     'content'     =>
        '<!-- wp:group -->
 		<div class="wp-block-group"><!-- wp:generateblocks/headline {"uniqueId":"2e0f032a","alignment":"left","textColor":"","borderColor":"var(\u002d\u002dglobal-color-10)","highlightTextColor":"","paddingLeft":"20","borderSizeLeft":"6","iconColor":"var(\u002d\u002dglobal-color-10)","iconSize":34,"iconSizeUnit":"px"} -->
@@ -326,7 +326,7 @@ register_block_pattern(
     array(
     'title' => __( 'Überschrift H2 mit Text', 'ueberschrift_h2_mit_text' ),
     'description' => _x( 'Überschrift H2 mit Text', 'Überschrift H2 mit Text', 'ueberschrift_h2_mit_text' ),
-    'categories'  => array('Custom Websites Haurand'),
+    'categories'  => array('EKS Haurand'),
     'content'     =>
        '<!-- wp:group -->
 		<div class="wp-block-group"><!-- wp:generateblocks/headline {"uniqueId":"dcce5854","alignment":"left","textColor":"","borderColor":"var(\u002d\u002dglobal-color-10)","highlightTextColor":"","paddingLeft":"20","borderSizeLeft":"6","iconColor":"var(\u002d\u002dglobal-color-10)","iconSize":34,"iconSizeUnit":"px"} -->
@@ -350,7 +350,7 @@ register_block_pattern(
     array(
     'title' => __( 'Zwei Spalten mit Texten und H2', 'zwei_spalten_mit_texten_und_h2' ),
     'description' => _x( 'Zwei Spalten mit Texten und H2', 'Zwei Spalten mit Texten und H2', 'zwei_spalten_mit_texten_und_h2' ),
-    'categories'  => array('Custom Websites Haurand'),
+    'categories'  => array('EKS Haurand'),
     'content'     =>
        '<!-- wp:group -->
 		<div class="wp-block-group"><!-- wp:generateblocks/headline {"uniqueId":"54b86bd7","alignment":"left","textColor":"","borderColor":"var(\u002d\u002dglobal-color-10)","highlightTextColor":"","paddingLeft":"20","borderSizeLeft":"6","iconColor":"var(\u002d\u002dglobal-color-10)","iconSize":34,"iconSizeUnit":"px"} -->
