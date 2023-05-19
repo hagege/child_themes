41c02ea514ece35fffeb66560d7b2d757a8ad862
hgg <postmaster@aachenerkinder.de>
2023-05-19 09:12:56 +0200

    jetzt werden die Wochenpläne von der vorherigen, aktueller und nächster Woche gezeigt

--- plugins/automatic_pdf_link/automatic_pdf_link.php
+++ plugins/automatic_pdf_link/automatic_pdf_link.php
@@ -3,7 +3,7 @@
 Plugin Name: Automatic PDF Link
 Plugin URI: http://haurand.com
 Description: A plugin to post automatic per shortcode an actual PDF-File as link
-Version: 1.0
+Version: 0.2
 Author: Hans-Gerd Gerhards
 Author URI: http://haurand.com
 License: GPLv2 or later
@@ -15,12 +15,11 @@ function wochenplan_shortcode(){
   $upload_dir = wp_upload_dir();
   /* https://seniorensport-attendorn.de/wp-content/uploads/2023/05/KW_21_Wochenplan_SeniorenSport.pdf */
   $week_number = date("W");
-  $week_number_before = $week_number - 1;
-  $week_number_after = $week_number + 1;
-  $out = '<a href="';
-  $out .= esc_url( $upload_dir['url'] . '/KW_' . $week_number . '_Wochenplan_SeniorenSport.pdf') . '">';
-  $out .= 'Aktuellen Wochenplan ansehen und herunterladen - KW - ' . $week_number;
-  $out .= '</a>';
-
-  return $out;
+  for ($i=$week_number-1; $i <= $week_number+1; $i++) { 
+	  $out .= '<li><a href="';
+	  $out .= esc_url( $upload_dir['url'] . '/KW_' . $i . '_Wochenplan_SeniorenSport.pdf') . '">';
+	  $out .= 'Aktuellen Wochenplan ansehen und herunterladen - KW - ' . $i;
+	  $out .= '</a></li>';
+  }
+  return $out; 
 }
--- plugins/automatic_pdf_link/readme.txt
+++ plugins/automatic_pdf_link/readme.txt
@@ -1,6 +1,6 @@
 === Automatic PDF Link ===
 Contributors: Hans-Gerd Gerhards, haurand.com
-Stable tag: 0.1
+Stable tag: 0.2
 Tested up to: 6.2.1
 Requires at least: 4.6
 
@@ -10,4 +10,6 @@ A plugin to post automatic per shortcode an actual PDF-File as link
 
 Weitere Optionen:
 1. Das Plugin sollte so erweitert werden, dass jeweils ein Link für die vorherige, aktuelle und nächste Woche gezeigt wird, wenn die Datei vorhanden ist.
+	> erledigt
 2. Das Plugin sollte einen Button für den Link erstellen, der per CSS-Regel entsprechend dargestellt wird. 
+3. Das Plugin sollte abfangen, wenn eine Datei nicht vorhanden ist
