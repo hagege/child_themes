d0cb78ccebacea47aff762b4d1368fece298a529
hgg <postmaster@aachenerkinder.de>
2023-05-28 10:01:31 +0200

    Kleinere Korrekturen und Versserungen am Design im Dashboard

--- plugins/custom_pdf_file_upload_to_folder/custom_pdf_file_upload_to_folder.php
+++ plugins/custom_pdf_file_upload_to_folder/custom_pdf_file_upload_to_folder.php
@@ -1,20 +1,26 @@
 <?php
 /*
-Plugin Name: Custom PDF-File Upload to Folder
-Description: Allows users to upload PDF-files to a custom folder from the dashboard menu.
-Version: 0.1
+Plugin Name: Custom PDF File Upload to Folder
+Plugin URI: http://haurand.com
+Description: A plugin to post automatic per shortcode an actual PDF-File as link
+Version: 0.1.1
 Author: Hans-Gerd Gerhards
 Author URI: http://haurand.com
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
+// Securing against unauthorized access //
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 // Add the custom menu option in the dashboard menu
 add_action('admin_menu', 'custom_file_upload_add_menu');
 function custom_file_upload_add_menu() {
     add_menu_page(
-        'Custom File Upload',
-        'Custom File Upload',
+        'PDF Wochenplan Upload',
+        'PDF Wochenplan Upload',
         'manage_options',
         'custom-file-upload',
         'custom_file_upload_page',
@@ -39,28 +45,57 @@ function custom_file_upload_page() {
         $file_type = wp_check_filetype(basename($file['name']), null);
 
         // Check if the file is allowed
-        $allowed_types = array('jpg', 'jpeg', 'png', 'gif', 'pdf');
+        /* $allowed_types = array('jpg', 'jpeg', 'png', 'gif', 'pdf'); */
+		$allowed_types = array('pdf');
         if (!in_array($file_type['ext'], $allowed_types)) {
-            echo 'Invalid file type. Only JPG, JPEG, PNG, PDF and GIF files are allowed.';
+            echo 'Invalid file type. Only PDF files are allowed.';
             return;
         }
 
         // Move the uploaded file to the custom folder
         if (move_uploaded_file($file['tmp_name'], $target_file)) {
-            echo 'File uploaded successfully.';
+            echo $target_file .' - File uploaded successfully.';
         } else {
-            echo 'Failed to upload the file.';
+            echo $target_file . ' - Failed to upload the file.';
         }
     }
 
     // Render the file upload form
     ?>
     <div class="wrap">
-        <h1>Custom File Upload</h1>
+        <h1>PDF-Datei hochladen</h1>
         <form enctype="multipart/form-data" method="post">
             <input type="file" name="custom_file" />
-            <input type="submit" name="custom_file_upload_submit" value="Upload" />
+            <input type="submit" name="custom_file_upload_submit" value="Upload (Hochladen)" />
         </form>
     </div>
     <?php
+}
+
+
+add_shortcode( 'wochenplan' , 'wochenplan_shortcode' );
+function wochenplan_shortcode(){
+  $upload_dir = wp_upload_dir();
+  $out = '';
+  /* https://seniorensport-attendorn.de/wp-content/uploads/2023/05/KW_21_Wochenplan_SeniorenSport.pdf */ 
+  $week_number = date("W");
+  for ($i=$week_number-1; $i <= $week_number+1; $i++) { 
+	  $upload_dir = wp_upload_dir(); // Get the default upload directory
+      $pdf_file = $upload_dir['baseurl'] . '/wochenplan/' . 'KW_' . $i . '_Wochenplan_SeniorenSport.pdf'; // Custom folder path
+	  $pdf_file_name = 'KW_' . $i . '_Wochenplan_SeniorenSport.pdf';
+   	  if(UR_exists($pdf_file)){ 
+		  $out .= '<div class="pdf-button"><a href="';
+		  $out .= esc_url( $pdf_file) . '">';
+		  $out .= 'KW - ' . $i . ' - Hier klicken';
+		  $out .= '</a></div>';
+	  } else {
+		  $out .= '<div class="pdf-button-not-found">Die Datei <strong>' . $pdf_file_name . '</strong> ist noch nicht oder nicht mehr vorhanden.</div>';
+	  }  
+  }
+  return $out;
+}
+
+function UR_exists($url){
+	   $headers=get_headers($url);
+	   return stripos($headers[0],"200 OK")?true:false;
 }
---
+++ plugins/custom_pdf_file_upload_to_folder/readme.txt
@@ -0,0 +1,37 @@
+=== Custom PDF-File Upload to Folder ===
+Contributors: Hans-Gerd Gerhards, haurand.com
+Stable tag: 0.1.1
+Tested up to: 6.2.2
+Requires at least: 4.6
+
+
+== Description ==
+
+A plugin to post automatic per shortcode an actual PDF-File as link with option to uplod PDF-Files
+
+Weitere Optionen:
+1. Das Plugin sollte so erweitert werden, dass jeweils ein Link für die vorherige, aktuelle und nächste Woche gezeigt wird, wenn die Datei vorhanden ist.
+	> erledigt
+2. Das Plugin sollte einen Button für den Link erstellen, der per CSS-Regel entsprechend dargestellt wird. 
+	> erledigt
+3. Das Plugin sollte abfangen, wenn eine Datei nicht vorhanden ist.
+	> erledigt
+4. Es sollte ein Ordner nur für den Upload der PDF-Dateien (wochenplan) gewählt werden können.
+    > erledigt. Allerdings wird der Ordner (wochenplan) fest vorgegeben
+5. für berechtigte Rollen muss eine Option zum Hochladen der Datei geschaffen werden.
+
+== change log ==
+
+28.5.2023: Upload-Möglichkeit mit Menüpunkt im Backend
+           Kleinere Korrekturen und Verbesserungen am Design des Dashboards 
+
+24.5.2023: Button-Beschriftung geändert. CSS-Klassen für gefundene und nicht gefundene Dateien. Dadurch bessere Darstellung möglich.
+
+22.5.2023: Warnung "$out nicht definiert" korrigiert.
+
+== Evtl. hilfreiche Links und Plugins: ==
+https://stackoverflow.com/questions/7684771/how-to-check-if-a-file-exists-from-a-url
+https://wordpress.org/plugins/custom-upload-folder/
+https://stackoverflow.com/questions/70715768/wordpress-specify-a-custom-upload-folder-for-a-plugin
+https://stackoverflow.com/questions/35147099/wordpress-upload-media-in-a-specific-folder-for-a-special-post
+https://stackoverflow.com/questions/10444059/file-exists-returns-false-even-if-file-exist-remote-url
