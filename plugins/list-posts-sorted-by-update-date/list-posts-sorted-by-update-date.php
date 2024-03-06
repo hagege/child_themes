959abc4819510f948d728f1b44a59740d10e561e
hgg <postmaster@aachenerkinder.de>
2024-03-06 11:15:27 +0100

    initial

---
+++ plugins/list-posts-sorted-by-update-date/list-posts-sorted-by-update-date.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * List Posts sorted by update date
+ *
+ * @package       LISTPOSTSS
+ * @author        Hans-Gerd Gerhards
+ * @license       gplv2
+ * @version       0.1
+ *
+ * @wordpress-plugin
+ * Plugin Name:   List Posts sorted by update date
+ * Plugin URI:    https://haurand.com
+ * Description:   the posts are listed sorted by update date in \"All posts\". 
+ * Version:       0.1
+ * Author:        Hans-Gerd Gerhards
+ * Author URI:    https://haurand.com
+ * Text Domain:   list-posts-sorted-by-update-date
+ * Domain Path:   /languages
+ * License:       GPLv2
+ * License URI:   https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with List Posts sorted by update date. If not, see <https://www.gnu.org/licenses/gpl-2.0.html/>.
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Include your custom code here.
+
+/*
+
+function custom_posts_orderby( $query ) {
+    if ( is_admin() && $query->is_main_query() && $query->get('post_type') == 'post' ) {
+        $query->set( 'orderby', 'modified' );
+        $query->set( 'order', 'DESC' );
+    }
+}
+add_action( 'pre_get_posts', 'custom_posts_orderby' );
+*/
+
+function custom_posts_orderby( $query ) {
+    if ( is_admin() && $query->is_main_query() && $query->get('post_type') == 'post' ) {
+        $orderby_options = array(
+            'date'       => 'Date',
+            'title'      => 'Title',
+            'modified'   => 'Last Modified',
+        );
+
+        // Set default orderby value
+        $default_orderby = 'modified';
+
+        $orderby_options = apply_filters( 'custom_posts_orderby_options', $orderby_options );
+
+        $orderby = isset($_GET['orderby']) && in_array($_GET['orderby'], array_keys($orderby_options)) ? $_GET['orderby'] : $default_orderby;
+
+        $query->set( 'orderby', $orderby );
+    }
+}
+add_action( 'pre_get_posts', 'custom_posts_orderby' );
+
+function custom_posts_orderby_options( $orderby_options ) {
+    // Add more orderby options if needed
+    $orderby_options['date'] = 'Date';
+    $orderby_options['title'] = 'Title';
+    $orderby_options['modified'] = 'Last Modified';
+    return $orderby_options;
+}
+add_filter( 'custom_posts_orderby_options', 'custom_posts_orderby_options' );
+
+
