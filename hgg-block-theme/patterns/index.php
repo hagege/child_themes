74f358f93d89fcf74dc004b21c625acce16b11dc
hgg <postmaster@aachenerkinder.de>
2024-09-01 21:23:47 +0200

    Skio to content korrigiert, Footer korrigiert

--- hgg-block-theme/patterns/index.php
+++ hgg-block-theme/patterns/index.php
@@ -6,11 +6,7 @@
  * Inserter: no
  */
 ?>
-<!-- wp:template-part {"slug":"header-2"} /-->
-
-<!-- wp:paragraph -->
-<p><?php esc_html_e('/tr', 'hgg-block-theme');?></p>
-<!-- /wp:paragraph -->
+<!-- wp:template-part {"slug":"neuer-header","area":"header"} /-->
 
 <!-- wp:separator -->
 <hr class="wp-block-separator has-alpha-channel-opacity"/>
@@ -20,9 +16,9 @@
 <div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide"><!-- wp:query {"queryId":9,"query":{"perPage":6,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"metadata":{"categories":["posts"],"patternName":"core/query-standard-posts","name":"Standard"},"align":"wide"} -->
+<!-- wp:group {"style":{"background":{"backgroundImage":{"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/circle_light_orange.svg","id":6935,"source":"file","title":"circle_light_orange"},"backgroundPosition":"50% 0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"tagName":"main","align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"background":{"backgroundPosition":"50% 0"}}} -->
+<main class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:query {"queryId":9,"query":{"perPage":6,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"metadata":{"categories":["posts"],"patternName":"core/query-standard-posts","name":"Standard"},"align":"wide"} -->
 <div class="wp-block-query alignwide"><!-- wp:post-template {"layout":{"type":"grid","columnCount":"2"}} -->
 <!-- wp:post-title {"isLink":true} /-->
 
@@ -48,7 +44,7 @@
 
 <!-- wp:query-pagination-next /-->
 <!-- /wp:query-pagination --></div>
-<!-- /wp:query --></div>
+<!-- /wp:query --></main>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
@@ -56,4 +52,4 @@
 <hr class="wp-block-separator has-alpha-channel-opacity"/>
 <!-- /wp:separator -->
 
-<!-- wp:template-part {"slug":"footer"} /-->
+<!-- wp:template-part {"slug":"neuer-footer","area":"footer"} /-->
--- hgg-block-theme/patterns/neuer-footer.php
+++ hgg-block-theme/patterns/neuer-footer.php
@@ -6,12 +6,12 @@
  * Inserter: no
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"}} -->
+<!-- wp:group {"layout":{"type":"default"}} -->
 <div class="wp-block-group"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:site-title {"level":2,"fontSize":"medium"} /-->
 
 <!-- wp:paragraph -->
-<p><?php esc_html_e('Proudly powered by <a href="https://wordpress.org">WordPress</a>', 'hgg-block-theme');?></p>
+<p><?php esc_html_e('Proudly powered by WordPress', 'hgg-block-theme');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
