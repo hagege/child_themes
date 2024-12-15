9e31bcbf2007a45a59264293fcda0c96205dcb25
hgg <postmaster@aachenerkinder.de>
2024-12-15 13:39:56 +0100

    Search accessible in card (wp go maps)

--- plugins/add-column-posts-modified-date/add-column-posts-modified-date.php
+++ plugins/add-column-posts-modified-date/add-column-posts-modified-date.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Add Column Posts Modified Date 
+ * Add Column Posts Modified Date
  *
- * @package       ADDCOLUMNP
- * @author        Hans-Gerd Gerhards
- * @license       gplv2
- * @version       0.3
+ * @package ADDCOLUMNP
+ * @author  "Hans-Gerd Gerhards<gerhards@haurand.com>"
+ * @license gplv2
+ * @version 0.3
  *
  * @wordpress-plugin
- * Plugin Name:   Add Column Posts Modified Date 
+ * Plugin Name:   Add Column Posts Modified Date
  * Plugin URI:    https://haurand.com
  * Description:   Add a “Last Updated” Column To The WordPress Backend (Works for Posts & Pages)
  * Version:       0.3
@@ -24,73 +24,80 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
 // Include your custom code here.
 
-// Adds a new sortable "last updated" column to posts and pages backend.
-// Source: https://mintedempire.com/add-a-last-updated-column-to-the-wp-backend/
-function custom_columns($defaults) {
+/**
+ * Adds a new sortable "last updated" column to posts and pages backend. Source: https://mintedempire.com/add-a-last-updated-column-to-the-wp-backend/
+ */
+function Custom_columns($defaults)
+{
     $defaults['last_updated'] = __('Last Updated', 'your-textdomain');
     return $defaults;
 }
 add_filter('manage_posts_columns', 'custom_columns');
-// add_filter('manage_pages_columns', 'custom_columns');
 
-function custom_columns_content($column_name, $post_id) {
+/** add_filter('manage_pages_columns', 'custom_columns'); */
+function Custom_Columns_content($column_name, $post_id)
+{
     if ($column_name == 'last_updated') {
         $last_updated = get_the_modified_date("d.m.Y - H:i");
         echo esc_html__($last_updated) . '<br>';
-		if ( !empty( get_the_modified_author() ) ) {
-			echo esc_html__( 'by', 'show-modified-date-in-admin-lists' ) . ' <strong>'.get_the_modified_author().'<strong>';
-		} else {
-			echo esc_html__( 'by', 'show-modified-date-in-admin-lists' ) . ' <strong>' . esc_html__( 'UNKNOWN', 'show-modified-date-in-admin-lists' ) . '<strong>';
-		}
+        if (!empty(get_the_modified_author())) {
+            echo esc_html__('by', 'show-modified-date-in-admin-lists') . ' <strong>' . get_the_modified_author() . '<strong>';
+        } else {
+            echo esc_html__('by', 'show-modified-date-in-admin-lists') . ' <strong>' . esc_html__('UNKNOWN', 'show-modified-date-in-admin-lists') . '<strong>';
+        }
     }
 }
 add_action('manage_posts_custom_column', 'custom_columns_content', 10, 2);
 // add_action('manage_pages_custom_column', 'custom_columns_content', 10, 2);
 
-function custom_columns_sortable($columns) {
+function Custom_Columns_sortable($columns)
+{
     $columns['last_updated'] = 'last_updated';
     return $columns;
 }
 add_filter('manage_edit-post_sortable_columns', 'custom_columns_sortable');
 // add_filter('manage_edit-page_sortable_columns', 'custom_columns_sortable');
 
-function custom_columns_orderby($query) {
+function Custom_Columns_orderby($query)
+{
     if (!is_admin()) {
         return;
     }
 
     $orderby = $query->get('orderby');
-	// echo "Hier kommt der Wert der Variablen: " . var_dump($orderby);
-    
+    // echo "Hier kommt der Wert der Variablen: " . var_dump($orderby);
+
     if ('last_updated' == $orderby) {
         $query->set('orderby', 'modified');
     }
 }
 add_action('pre_get_posts', 'custom_columns_orderby');
 
-
 /* Sort posts in wp_list_table by column in ascending or descending order. */
-function custom_post_order($query){
-    /* 
-        Set post types.
-        _builtin => true returns WordPress default post types. 
-        _builtin => false returns custom registered post types. 
-    */
+function Custom_Post_order($query)
+{
+    /*
+    Set post types.
+    _builtin => true returns WordPress default post types.
+    _builtin => false returns custom registered post types.
+     */
     $post_types = get_post_types(array('_builtin' => true), 'names');
     /* The current post type. */
     $post_type = $query->get('post_type');
     /* Check post types. */
-    if(in_array($post_type, $post_types)){
+    if (in_array($post_type, $post_types)) {
         /* Post Column: e.g. title */
-        if($query->get('orderby') == ''){
+        if ($query->get('orderby') == '') {
             $query->set('orderby', 'Last Updated');
         }
         /* Post Order: ASC / DESC */
-        if($query->get('order') == ''){
+        if ($query->get('order') == '') {
             $query->set('order', 'DESC');
         }
     }
--- plugins/disable-admin-bar-in-maintenance-mode/disable-admin-bar-in-maintenance-mode.php
+++ plugins/disable-admin-bar-in-maintenance-mode/disable-admin-bar-in-maintenance-mode.php
@@ -2,10 +2,10 @@
 /**
  * Disable Admin Bar in Maintenance Mode
  *
- * @package       DISABLEADM
- * @author        Hans-Gerd Gerhards
- * @license       gplv2
- * @version       0.1.1
+ * @package DISABLEADM
+ * @author Hans-Gerd Gerhards
+ * @license gplv2
+ * @version 0.1.1
  *
  * @wordpress-plugin
  * Plugin Name:   Disable Admin Bar in Maintenance Mode
@@ -24,31 +24,36 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
 // Add settings to the WordPress admin menu
-function maintenance_mode_admin_bar_menu() {
+function maintenance_mode_admin_bar_menu()
+{
     add_options_page('Maintenance Mode Admin Bar Settings', 'Maintenance Mode Admin Bar', 'manage_options', 'maintenance-mode-admin-bar', 'maintenance_mode_admin_bar_page');
 }
 
 add_action('admin_menu', 'maintenance_mode_admin_bar_menu');
 
 // Callback function to render the settings page
-function maintenance_mode_admin_bar_page() {
+function maintenance_mode_admin_bar_page()
+{
     ?>
     <div class="wrap">
         <h1>Maintenance Mode Admin Bar Settings</h1>
         <form method="post" action="options.php">
-            <?php settings_fields('maintenance_mode_admin_bar_settings_group'); ?>
-            <?php do_settings_sections('maintenance-mode-admin-bar'); ?>
-            <?php submit_button(); ?>
+            <?php settings_fields('maintenance_mode_admin_bar_settings_group');?>
+            <?php do_settings_sections('maintenance-mode-admin-bar');?>
+            <?php submit_button();?>
         </form>
     </div>
     <?php
 }
 
 // Register settings and fields
-function maintenance_mode_admin_bar_settings() {
+function maintenance_mode_admin_bar_settings()
+{
     register_setting('maintenance_mode_admin_bar_settings_group', 'disable_admin_bar');
 
     add_settings_section('maintenance_mode_admin_bar_main_section', 'Admin Bar Settings', 'maintenance_mode_admin_bar_section_text', 'maintenance-mode-admin-bar');
@@ -59,19 +64,22 @@ function maintenance_mode_admin_bar_settings() {
 add_action('admin_init', 'maintenance_mode_admin_bar_settings');
 
 // Section text callback
-function maintenance_mode_admin_bar_section_text() {
+function maintenance_mode_admin_bar_section_text()
+{
     echo '<p>Configure admin bar settings during maintenance mode.</p>';
 }
 
 // Admin bar deactivated checkbox callback
-function disable_admin_bar_checkbox() {
+function disable_admin_bar_checkbox()
+{
     $disable_admin_bar = get_option('disable_admin_bar');
     echo '<input type="checkbox" id="disable_admin_bar" name="disable_admin_bar" value="1" ' . checked(1, $disable_admin_bar, false) . ' />';
-    echo '<label for="disable_admin_bar">Deactivate admin bar during maintenance mode</label>';    
+    echo '<label for="disable_admin_bar">Deactivate admin bar during maintenance mode</label>';
 }
 
 // Check if maintenance mode is active and admin bar should be deactivated
-function check_maintenance_mode() {
+function check_maintenance_mode()
+{
     $disable_admin_bar = get_option('disable_admin_bar');
     $maintenance_mode_active = get_option('maintenance_mode_active');
 
