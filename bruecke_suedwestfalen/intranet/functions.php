0e39b9418d8a9b2f93de63b3269ce078bab6195d
hgg <postmaster@aachenerkinder.de>
2025-02-12 06:12:29 +0100

    initial child theme

---
+++ bruecke_suedwestfalen/intranet/functions.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * GeneratePress child theme functions and definitions.
+ *
+ * Add your custom PHP in this file.
+ * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
+ */
+
+add_theme_support( 'appearance-tools' );
+
+/*
+add_action( 'init', 'themeslug_enqueue_block_styles' );
+
+function themeslug_enqueue_block_styles() {
+   wp_enqueue_block_style(
+       'core/cover',
+       array(
+           'handle' => 'themeslug--card-interactive',
+           'src'    => get_theme_file_uri( 'assets/css/blocks/core/cover-card-interactive.css' ),
+           'path'   => get_theme_file_path( 'assets/css/blocks/core/cover-card-interactive.css' ),
+       )
+   );
+}
+
+add_action( 'init', 'themeslug_register_block_styles' );
+
+function themeslug_register_block_styles() {
+   register_block_style(
+       'core/cover',
+       array(
+           'name'         => 'card--interactive',
+           'label'        => __( 'Card (Interactive)', 'themeslug' ),
+           'style_handle' => 'themeslug--card-interactive',
+       )
+   );
+}
+*/
