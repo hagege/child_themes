f11fddc375deeff99396a448f9a7c3491f6abb6f
hgg <postmaster@aachenerkinder.de>
2025-05-30 11:22:16 +0200

    preparing version 1.3

---
+++ plugins/shrinking-logo-sticky-header/version_1_3/readme.txt
@@ -0,0 +1,105 @@
+=== Dynamic Header & Navigation for Block Themes ===
+Author URI: https://haurand.com/
+Plugin URI: https://haurand.com/plugin-shrinking-logo-sticky-header/
+Donate link: 
+Contributors: @hage
+Tags: Block Theme, Header, shrink, breakpoint, off-canvas
+Requires at least: 
+Tested up to: 6.8
+Requires PHP: 7.4
+Stable tag: 1.2
+License: GPLv2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Animated shrinking header, responsive shrinking logo, custom breakpoints and off-canvas navigation – navigation solution for block themes.
+
+== Description ==
+
+* Set height for header
+* Set height for shrinking header
+* Set Logo shrinking factor 
+* Set Animation duration (seconds) for shrinking header
+* Option to set a breakpoint for navigation (added in Version 1.1)
+* Option to set Off-Canvas (added in Version 1.2)
+
+Dynamic Header & Navigation for Block Themes (Previous name of the plugin: Shrinking Logo Sticky Header) is a lightweight WordPress plugin developed specifically as a navigation solution for most Block Themes. 
+It adds a modern sticky header with smooth, animated shrinking effects for both the header and the site logo. 
+As users scroll down the page, the header and logo automatically reduce in size, maximizing on-screen space and keeping navigation easily accessible without disrupting the browsing experience.
+
+The plugin allows you to individually configure the heights for the header and logo in both their normal and shrunken states, giving you full control over your site’s appearance. 
+These settings can be easily adjusted to match your branding or design preferences.
+
+One of the key advantages of Dynamic Header & Navigation for Block Themes is its simplicity: 
+After activation, the sticky and shrinking effects work in principle immediately - no additional setup or configuration is required. 
+This makes it ideal for users who want a professional, dynamic header effect without dealing with complex options or custom code.
+
+
+== Frequently Asked Questions ==
+= Is it possible to use this plugin also for classical Themes (e. g. Astra, OceanWP, GeneratePress)? =
+
+Classic themes sometimes offer such an option, but possibly only in a paid version. This plugin can only be used with block themes.
+
+= Is it possible to use this plugin for all Block Themes? =
+Tested with Twenty Twenty-Three, Twenty Twenty-Four, Twenty Twenty-Five, Circles WP. 
+If the plugin does not work with your block theme, please write to the support forum. I will then see what I can do. 
+
+= Why is the sticky header transparent and how can I change this? =
+
+The template part for the header is initially transparent in many block themes. 
+For this reason, the background colour of the outer group block for the header must be changed to a colour such as white, for example. 
+If the header has a background colour, then the header ‘covers’ the content when scrolling. See also the screenshot.
+
+= Can you show me examples that demonstrate these effects? =
+
+Yes, with pleasure:
+https://haurand.com/  and  https://space4.yd-sgs.de/
+
+
+== Installation ==
+
+1. Go to `Plugins` in the Admin menu
+2. Click on the button `Add new`
+3. Search for `shrinking Logo Sticky Header` and click 'Install Now' or click on the `upload` link to upload `shrinking-logo-sticky-header.zip`
+4. Click on `Activate plugin`
+
+== Changelog ==
+
+= 1.2: May 22, 2025 =
+* Added: Optional Off-Canvas-Menu
+* Added: Uninstall: Delete the Options in Table when unistalling this Plugin
+* Renaming the Plugin: "Dynamic Header & Navigation for Block Themes" instead of previous name of the plugin: "Shrinking Logo Sticky Header"
+* Updating the language files
+* Fix: Loading language Files Notice: "This plugin is not properly prepared for localization."
+* Enhancements: Code improvement (Delete superfluous option)
+
+= 1.1: May 9, 2025 =
+* Added: Optional Breakpoint settings
+* Enhancements: Code improvement
+
+= 1.0: May 1, 2025 =
+* Enhancement: Security (Additional escaping of the values)
+* Enhancement: Rename the text domain due to Internationalize this plugin
+* Check with Plugin Check: "Check completed. No errors were found."
+
+= 0.4.4: April 26, 2025 =
+* Enhancement: Optimize CSS
+
+= 0.4.3: April 26, 2025 =
+* Added: Option for Logo shrinking factor (Value in 0.05 steps)
+* Enhancement: CSS optimised
+
+= 0.4.2: April 26, 2025 =
+* Added: Translation option and Translation files in language folder
+* Added: Option for Header Height
+
+= 0.4.1: April 25, 2025 =
+* Fix: Issue with inner group
+
+= 0.4: April 25, 2025 =
+* Added Settings in Backend for Heigth of Header/Logo, Animation duration
+
+= 0.3: April 24, 2025 =
+* Optimized shrinking effect
+
+= 0.1: April 24, 2025 =
+* Birthday of shrinking Logo Sticky Header
---
+++ plugins/shrinking-logo-sticky-header/version_1_3/shrinking-logo-sticky-header.php
@@ -0,0 +1,344 @@
+<?php
+/**
+ * Shrinking Logo Sticky Header
+ *
+ * @package       shrinkingLO
+ * @author        Hans-Gerd Gerhards
+ * @license       gplv2
+ * @version       1.3
+ *
+ * @wordpress-plugin
+ * Plugin Name:   Dynamic Header & Navigation for Block Themes
+ * Plugin URI:    https://haurand.com/plugin-shrinking-logo-sticky-header/
+ * Description:   Animated shrinking header, responsive shrinking logo, custom breakpoints and off-canvas navigation – all-in-one navigation solution for most modern WordPress block themes.
+ * Version:       1.3
+ * Author:        Hans-Gerd Gerhards
+ * Author URI:    https://haurand.com/author/hgg/
+ * Text Domain:   shrinking-logo-sticky-header
+ * Domain Path:   /languages
+ * License:       GPLv2
+ * License URI:   https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with shrinking Logo Sticky Header. If not, see <https://www.gnu.org/licenses/gpl-2.0.html/>.
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// set version.
+const SLSH_VERSION = '1.3.0';
+
+/**
+ * Load language files.
+ *
+ * @return void
+ */
+function slsh_load_textdomain(): void {
+    load_plugin_textdomain(
+        'shrinking-logo-sticky-header',
+        false,
+        dirname( plugin_basename( __FILE__ ) ) . '/languages/'
+    );
+}
+add_action( 'init', 'slsh_load_textdomain' );
+
+
+/**
+ * Registering settings
+ */
+function slsh_register_settings(): void {
+	add_option( 'slsh_header_shrink_height', 80 );
+	add_option( 'slsh_animation_duration', 0.6 );
+	add_option( 'slsh_heigth_header', 120 );
+	add_option( 'slsh_logo_in_header_shrink_height', 0.8 );
+
+	// Options for Breakpoint.
+	add_option( 'slsh_nav_breakpoint', 782 );
+	
+	// Delete superfluous option 
+	delete_option( 'slsh_enable_nav_css' );
+	
+	// Option for Off-Canvas-Menue
+	add_option( 'slsh_enable_off_canvas', 'no' );
+	
+	// Option for Submenue
+	add_option( 'slsh_enable_submenu', 'no' );	
+
+	register_setting(
+		'slsh_options_group',
+		'slsh_header_shrink_height',
+		array(
+			'type'              => 'integer',
+			'sanitize_callback' => 'absint',
+		)
+	);
+	register_setting(
+		'slsh_options_group',
+		'slsh_animation_duration',
+		array(
+			'type'              => 'float',
+			'sanitize_callback' => 'floatval',
+		)
+	);
+	register_setting(
+		'slsh_options_group',
+		'slsh_heigth_header',
+		array(
+			'type'              => 'integer',
+			'sanitize_callback' => 'absint',
+		)
+	);
+	register_setting(
+		'slsh_options_group',
+		'slsh_logo_in_header_shrink_height',
+		array(
+			'type'              => 'float',
+			'sanitize_callback' => 'floatval',
+		)
+	);
+	register_setting(
+		'slsh_options_group',
+		'slsh_nav_breakpoint',
+		array(
+			'type'              => 'integer',
+			'sanitize_callback' => 'absint',
+		)
+	);
+	register_setting(
+		'slsh_options_group',
+		'slsh_enable_off_canvas',
+		array(
+			'type'              => 'string',
+			'sanitize_callback' => function ( $v ) {
+				return 'yes' === $v ? 'yes' : 'no';
+			},
+		)
+	);
+	register_setting(
+		'slsh_options_group',
+		'slsh_enable_submenu',
+		array(
+			'type'              => 'string',
+			'sanitize_callback' => function ( $v ) {
+				return 'yes' === $v ? 'yes' : 'no';
+			},
+		)
+	);	
+}
+add_action( 'admin_init', 'slsh_register_settings' );
+
+/**
+ * Settings page in admin menu
+ */
+function slsh_register_options_page(): void {
+	add_options_page( 'Dynamic Header & Navigation for Block Themes', 'Dynamic Header & Navigation for Block Themes', 'manage_options', 'slsh', 'slsh_options_page' );
+}
+add_action( 'admin_menu', 'slsh_register_options_page' );
+
+/**
+ * Show options page.
+ *
+ * @return void
+ */
+function slsh_options_page(): void {
+	?>
+	<div>
+		<h2><?php esc_html_e( 'Dynamic Header & Navigation for Block Themes – Settings', 'shrinking-logo-sticky-header' ); ?></h2>
+		<form method="post" action="<?php echo esc_url( get_admin_url() ); ?>options.php">
+			<?php settings_fields( 'slsh_options_group' ); ?>
+			<table>
+				<tr>
+					<th scope="row"><label style="display: block; text-align: left" for="slsh_header_shrink_height"><?php esc_html_e( 'Height of the shrunk header (px):', 'shrinking-logo-sticky-header' ); ?></label></th>
+					<td><input type="number" id="slsh_header_shrink_height" name="slsh_header_shrink_height" value="<?php echo esc_attr( get_option( 'slsh_header_shrink_height', 80 ) ); ?>" min="40" max="300" /></td>
+				</tr>
+				<tr>
+					<th scope="row"><label style="display: block; text-align: left" for="slsh_animation_duration"><?php esc_html_e( 'Animation duration (seconds):', 'shrinking-logo-sticky-header' ); ?></label></th>
+					<td><input type="number" step="0.05" id="slsh_animation_duration" name="slsh_animation_duration" value="<?php echo esc_attr( get_option( 'slsh_animation_duration', 0.6 ) ); ?>" min="0.05" max="3" /></td>
+				</tr>
+				<tr>
+					<th scope="row"><label style="display: block; text-align: left" for="slsh_heigth_header"><?php esc_html_e( 'Normal height of header (px):', 'shrinking-logo-sticky-header' ); ?></label></th>
+					<td><input type="number" id="slsh_heigth_header" name="slsh_heigth_header" value="<?php echo esc_attr( get_option( 'slsh_heigth_header', 120 ) ); ?>" min="40" max="300" /></td>
+				</tr>
+				<tr>
+					<th scope="row"><label style="display: block; text-align: left" for="slsh_logo_in_header_shrink_height"><?php esc_html_e( 'Logo shrinking factor (Value in 0.05 steps):', 'shrinking-logo-sticky-header' ); ?></label></th>
+					<td><input type="number" step="0.05" id="slsh_logo_in_header_shrink_height" name="slsh_logo_in_header_shrink_height" value="<?php echo esc_attr( get_option( 'slsh_logo_in_header_shrink_height', 0.8 ) ); ?>" min="0.4" max="1" /></td>
+				</tr>
+
+				<!-- settings for breakpoint (CSS) -->
+				<tr>
+					<th scope="row"><h3 style="text-align: left; margin-top:30px">Breakpoint</h3></th>
+				</tr>
+				<tr>
+					<th scope="row"><p style="text-align: left;"><?php esc_html_e( 'A breakpoint is a screen width where the website layout changes to adapt for different devices like mobiles or desktops.', 'shrinking-logo-sticky-header' ); ?></p></th>
+				</tr>
+				<tr>
+					<th scope="row"><label style="display: block; text-align: left" for="slsh_nav_breakpoint"><?php esc_html_e( 'Breakpoint for Navigation (px):', 'shrinking-logo-sticky-header' ); ?></label></th>
+					<td>
+						<input type="number" id="slsh_nav_breakpoint" name="slsh_nav_breakpoint" value="<?php echo esc_attr( get_option( 'slsh_nav_breakpoint', 782 ) ); ?>" min="782" max="1920" />
+						<span><?php esc_html_e( 'Standard: 782', 'shrinking-logo-sticky-header' ); ?></span>
+					</td>
+				</tr>
+				<!-- settings for Off-Canvas (CSS) -->
+				<tr>
+					<th scope="row"><h3 style="text-align: left;margin-top:30px">Off-Canvas-Menu</h3></th>
+				</tr>
+				<tr>
+					<th scope="row"><p style="text-align: left;"><?php esc_html_e( 'Off-canvas is a hidden navigation panel that slides in from the side of the screen, providing a space-saving way to access menu options without cluttering the main content area.', 'shrinking-logo-sticky-header' ); ?></p></th>
+				</tr>
+				<tr>
+					<th scope="row"><label style="display: block; text-align: left" for="slsh_enable_off_canvas"><?php esc_html_e( 'Activate Off-Canvas (CSS Rules):', 'shrinking-logo-sticky-header' ); ?></label></th>
+					<td>
+						<input type="checkbox" id="slsh_enable_off_canvas" name="slsh_enable_off_canvas" value="yes" <?php checked( 'yes', get_option( 'slsh_enable_off_canvas', 'no' ) ); ?> />
+						<span><?php esc_html_e( 'Activates special CSS rules for Off-Canvas in Block Themes.', 'shrinking-logo-sticky-header' ); ?></span>
+					</td>
+				</tr>
+				<!-- settings for Submenu -->
+				<tr>
+					<th scope="row"><h3 style="text-align: left;margin-top:30px">Expandable submenu</h3></th>
+				</tr>
+				<tr>
+					<th scope="row"><label style="display: block; text-align: left" for="slsh_enable_submenu"><?php esc_html_e( 'Should it be possible to expand submenus?', 'shrinking-logo-sticky-header' ); ?></label></th>
+					<td>
+						<input type="checkbox" id="slsh_enable_submenu" name="slsh_enable_submenu" value="yes" <?php checked( 'yes', get_option( 'slsh_enable_submenu', 'no' ) ); ?> />
+						<span><?php esc_html_e( 'Should it be possible to expand submenus?', 'shrinking-logo-sticky-header' ); ?></span>
+					</td>
+				</tr>
+
+			</table>
+			<?php submit_button(); ?>
+		</form>
+	</div>
+	<?php
+}
+
+/**
+ * Load Dynamic CSS for Plugin Shrinking Logo Sticky Header
+ */
+function slsh_dynamic_css(): void {
+	// include css file.
+	wp_enqueue_style(
+		'slsh_style',
+		plugins_url( 'assets/css/shrinking-logo-sticky-header-style.css', __FILE__ ),
+		array(),
+		SLSH_VERSION
+	);
+	// variables.
+	$shrink_height      = (int) get_option( 'slsh_header_shrink_height', 80 );
+	$anim_duration      = (float) get_option( 'slsh_animation_duration', 0.6 );
+	$header_height      = (int) get_option( 'slsh_heigth_header', 120 );
+	$logo_shrink_height = (float) get_option( 'slsh_logo_in_header_shrink_height', 0.8 );
+	$nav_breakpoint     = (int) get_option( 'slsh_nav_breakpoint', 782 );
+	$enable_off_canvas  = get_option( 'slsh_enable_off_canvas', 'no' );
+	$enable_submenu     = get_option( 'slsh_enable_submenu', 'no' );
+
+	$custom_css = "
+        /* debug */
+		/* {$enable_submenu}; */
+		header.wp-block-template-part {
+            position: sticky;
+            top: 0;
+            z-index: 1000;
+            transition: height {$anim_duration}s cubic-bezier(.4,0,.2,1), background-color {$anim_duration}s;
+            height: {$header_height}px; 
+        }
+        header.wp-block-template-part.shrink {
+            height: {$shrink_height}px;
+        }
+        header.wp-block-template-part .wp-block-group {
+            height: 100%;
+        }
+        header.wp-block-template-part .wp-block-site-logo img {
+            transition: transform {$anim_duration}s cubic-bezier(0.4,0,0.2,1), height {$anim_duration}s cubic-bezier(0.4,0,0.2,1);
+            transform: scale(1);
+        }
+        header.wp-block-template-part.shrink .wp-block-site-logo img {
+            transform: scale({$logo_shrink_height});
+        }
+    ";
+
+	if ( $nav_breakpoint > 782 ) {
+		$custom_css .= "
+        @media screen and (max-width: {$nav_breakpoint}px) {
+            .wp-block-navigation__responsive-container-open {
+                display: block !important;
+            }
+            .wp-block-navigation__responsive-container:not(.is-menu-open.has-modal-open) {
+                display: none !important;
+            }
+        }";
+	}
+	
+	if ( 'yes' === $enable_off_canvas ) {
+		$custom_css .= "
+		/* Off canvas */
+		@media (max-width: {$nav_breakpoint}px) {
+		  .wp-block-navigation__responsive-container {
+			right: -70vw;
+			left: auto;
+			width: 70vw;
+			transition: none; 
+			border-radius: 0;
+		}
+
+		.wp-block-navigation__responsive-container.is-menu-open {
+			animation: slideInMenu 0.5s linear forwards;
+		}
+		@keyframes slideInMenu {
+			from {
+			  right: -70vw;
+			}
+			to {
+			  right: 0vw;
+			}
+		  }
+	    }";
+	}
+	
+	if ( 'yes' === $enable_submenu ) {
+		$custom_css .= "
+		/* Enable expandable Submenu */
+		/* Untermenüs ausblenden */
+		.wp-block-navigation__responsive-container .wp-block-navigation-submenu .wp-block-navigation__submenu-container {
+		  display: none;
+		}
+		
+		.wp-block-navigation-submenu .wp-block-navigation-item__content::after {
+		  content: '>';
+		  margin-left: 0.5em;
+		  font-size: 0.8em;
+	    }";
+	}
+
+	wp_add_inline_style( 'slsh_style', $custom_css );
+}
+add_action( 'wp_enqueue_scripts', 'slsh_dynamic_css' );
+
+/**
+ * Enqueue script.
+ *
+ * @return void
+ */
+function slsh_js_enqueue(): void {
+	wp_enqueue_script(
+		'slsh_js',
+		plugins_url( 'assets/js/slsh.js', __FILE__ ),
+		array(),
+		SLSH_VERSION,
+		true
+	);
+}
+add_action( 'wp_enqueue_scripts', 'slsh_js_enqueue' );
+
+function slsh_submenu_enqueue(): void {
+	wp_enqueue_script(
+		'slsh_submenu_js',
+		plugins_url( 'assets/js/slsh_submenu.js', __FILE__ ),
+		array(),
+		SLSH_VERSION,
+		true
+	);
+}
+add_action( 'wp_enqueue_scripts', 'slsh_submenu_enqueue' );
