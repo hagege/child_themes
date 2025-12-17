ba92a7687a1f3efd4470fadcccb35ff82fe45f22
hgg <postmaster@aachenerkinder.de>
2025-12-17 17:30:05 +0100

    initial child theme ollie

---
+++ bubenheim/ollie_child_theme/patterns/header-startseite.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Title: Header Startseite
+ * Slug: ollie-child/header-startseite
+ * Categories: Bubenheim
+ */
+?>
+<!-- wp:group {"align":"full","className":"gruppe-mit-cover","style":{"dimensions":{"minHeight":"100%"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull gruppe-mit-cover" style="min-height:100%"><!-- wp:cover {"url":"https://space7.yd-sgs.de/wp-content/uploads/2019/07/Kletterpark-1024x583.jpg","alt":"Kletterpark Bubenheimer Spieleland","dimRatio":0,"customOverlayColor":"#5e6a83","isUserOverlayColor":false,"minHeight":800,"minHeightUnit":"px","contentPosition":"center right","sizeSlug":"large","metadata":{"name":"Cover-Desktop"},"className":"cover-responsive"} -->
+<div class="wp-block-cover has-custom-content-position is-position-center-right cover-responsive" style="min-height:800px"><img class="wp-block-cover__image-background  size-large" alt="Kletterpark Bubenheimer Spieleland" src="https://space7.yd-sgs.de/wp-content/uploads/2019/07/Kletterpark-1024x583.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#5e6a83"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"7em","right":"7em"},"blockGap":"0"},"border":{"radius":"10px","left":{"color":"#ffffff00"}},"dimensions":{"minHeight":"100%"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-default" style="border-radius:10px;border-left-color:#ffffff00;min-height:100%;padding-top:var(--wp--preset--spacing--30);padding-right:7em;padding-bottom:var(--wp--preset--spacing--30);padding-left:7em"><!-- wp:group {"metadata":{"name":"Desktop-Gruppe"},"className":"desktop-gruppe","style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group desktop-gruppe" style="padding-top:0;padding-bottom:0"><!-- wp:paragraph {"style":{"color":{"text":"#ffffff"},"elements":{"link":{"color":{"text":"#ffffff"}}}},"fontSize":"xx-large"} -->
+<p class="has-text-color has-link-color has-xx-large-font-size" style="color:#ffffff"></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:spacer {"height":"1px","className":"negativer-spacer"} -->
+<div style="height:1px" aria-hidden="true" class="wp-block-spacer negativer-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"align":"full","className":"wave_startpage","style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"-10rem"}},"dimensions":{"minHeight":"0px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull wave_startpage" style="min-height:0px;margin-top:-10rem;padding-top:0;padding-bottom:0"><!-- wp:image {"sizeSlug":"full","linkDestination":"none","align":"full","className":"wave_startpage_image","style":{"color":[]}} -->
+<figure class="wp-block-image alignfull size-full wave_startpage_image"><img src="https://space7.yd-sgs.de/wp-content/uploads/2025/12/wave7a.svg" alt="" class=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ bubenheim/ollie_child_theme/patterns/spalte-mit-button.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Spalte mit Button
+ * Slug: ollie-child/spalte-mit-button
+ * Categories: 
+ */
+?>
+<!-- wp:columns {"className":"has-2-columns preis_spalte"} -->
+<div class="wp-block-columns has-2-columns preis_spalte"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p>Kinder ab 12 Jahren, die uns ohne ihre Eltern besuchen kommen, bringen bitte eine unterschriebene Einverständniserklärung mit.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:button {"style":{"color":{"text":"#ffffff","background":"#008000"}},"className":"aligncenter pre"} -->
+<div class="wp-block-button aligncenter pre"><a class="wp-block-button__link has-text-color has-background wp-element-button" href="https://space7.yd-sgs.de/wp-content/uploads/2019/07/Einverstaendniserklaerung_Kinder_ohne_Eltern.pdf" style="color:#ffffff;background-color:#008000">Einverständnis-<br>erklärung</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
