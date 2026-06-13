63bf3c798bd68f6ae201eb89cea00ffbb9c24dce
hgg <postmaster@aachenerkinder.de>
2026-06-13 06:38:46 +0200

    some enhancements

--- plugins/hgg-hidden-link/hidden-link-block.php
+++ plugins/hgg-hidden-link/hidden-link-block.php
@@ -1,53 +1,80 @@
 <?php
 /*
-Plugin Name: Hidden Link Block (PHP-Only Block)
-Description: Erstellt einen Block, mit dessen Hilfe ein unsichtbarer Link erstellt wird, 
-so dass die ganze Karte klickbar wird. Notwendig sind allerdings noch die folgenden CSS-Regeln.
+Plugin Name: Hidden Link Block
+Description: Erstellt einen Block, mit dessen Hilfe ein unsichtbarer Link erstellt wird, sodass eine komplette Karte klickbar wird.
+Version: 1.0
+Author: HGG
 */
 
+/**
+ * CSS laden
+ */
+function hgg_hidden_link_enqueue_styles() {
+
+	wp_enqueue_style(
+		'hgg-hidden-link-style',
+		plugin_dir_url( __FILE__ ) . 'style.css',
+		[],
+		filemtime( plugin_dir_path( __FILE__ ) . 'style.css' )
+	);
+
+}
+add_action( 'wp_enqueue_scripts', 'hgg_hidden_link_enqueue_styles' );
+
+
+/**
+ * Block registrieren
+ */
 function hgg_hidden_link_block_register_block() {
 
-    // Schritt 1: CSS registrieren (eigentlich in dem Fall nicht notwendig
-	add_action( 'wp_enqueue_scripts', function() {
-		wp_enqueue_style(
-			'hgg-hidden-link-style',
-			plugin_dir_url( __FILE__ ) . 'style.css',
-			[],
-			filemtime( plugin_dir_path( __FILE__ ) . 'style.css' )
-		);
-	} );
+	register_block_type(
+		'hgg-hidden-link/hgg-hidden-link-block',
+		[
+			'title'    => 'Hidden-Link-Block',
+			'icon'     => 'external',
+			'category' => 'text',
 
-    // Schritte 2 & 3: Block registrieren und autoRegister aktivieren. Color wäre nicht notwendig
-    register_block_type( 'hgg-hidden-link/hgg-hidden-link-block', [
-        'title'    => 'Hidden-Link-Block',
-		'icon'     => 'external',         // Externer Link
-        'category' => 'text',
-        'attributes' => [
-            'text' => [
-                'type'    => 'string',
-                'label'   => 'Hier bitte den Link eingeben:',
-                'default' => '',
-            ],
-        ],
-        'supports' => [
-            'autoRegister' => true,
-            'color' => [
-                'text'       => true,
-                'background' => true,
-            ],
-        ],
-        'render_callback' => 'hgg_hidden_link_block_render_block',
-    ] );
+			'attributes' => [
+				'url' => [
+					'type'    => 'string',
+					'label'   => 'Linkziel',
+					'default' => '',
+				],
+				'label' => [
+					'type'    => 'string',
+					'label'   => 'Aria-Label',
+					'default' => 'Karte öffnen',
+				],
+			],
+
+			'supports' => [
+				'autoRegister' => true,
+			],
+
+			'render_callback' => 'hgg_hidden_link_block_render_block',
+		]
+	);
+
 }
 add_action( 'init', 'hgg_hidden_link_block_register_block' );
 
-// Schritte 4 & 5: render_callback mit get_block_wrapper_attributes()
+
+/**
+ * Block ausgeben
+ */
 function hgg_hidden_link_block_render_block( $attributes ) {
-    // $wrapper = get_block_wrapper_attributes();
-    $text    = esc_url( $attributes['text'] ?? '' );
-	
-    return sprintf(
-    '<a class="karte-link" href="%s" aria-label="Zur Karte"></a>',
-    $text
-);
+
+	$url   = esc_url( $attributes['url'] ?? '' );
+	$label = esc_attr( $attributes['label'] ?? 'Karte öffnen' );
+
+	if ( empty( $url ) ) {
+		return '';
+	}
+
+	return sprintf(
+		'<a class="karte-link" href="%s" aria-label="%s"></a>',
+		$url,
+		$label
+	);
+
 }
